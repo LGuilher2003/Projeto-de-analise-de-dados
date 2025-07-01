395bda9374717225e381ca146aefc7e8c995b5ce
Luis Guilherme <guilherme1208moraes@gmail.com>
2025-07-01 18:05:41 -0300

    adicionando gráficos

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from lerDados import grafico_Produto_valor, grafico_Cidade_valor    
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def raiz():
+    return{"message": "Api de Gráficos de Vendas"}
 
-#ele busca o item com o id do item
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/grafico/Produto/Valor",response_class=HTMLResponse)
+def grafico_produto_valor():
+    """
+    Endpoint para obter o gráfico de valor total por produto.
+    Retorna a imagem do gráfico codificada em base64.
+    """
+    img = grafico_Produto_valor()
+    return f"<img src='data:image/png;base64,{img}'/>"
+   
+@app.get("/grafico/Cidade/Valor", response_class=HTMLResponse)
+def grafico_cidade_valor():
+    img  = grafico_Cidade_valor()
+    return f"<img src='data:image/png;base64,{img}'/>"
+#python -m uvicorn main:app --reload
